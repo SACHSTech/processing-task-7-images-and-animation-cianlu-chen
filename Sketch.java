9eb3fb3f63b84a0a15a3b8afcfb830af852dfdb9
cianlu-chen <kalina.chen25@ycdsbk12.ca>
2023-11-14 18:33:37 -0500

    the clouds???

--- Sketch.java
+++ Sketch.java
@@ -6,22 +6,28 @@ public class Sketch extends PApplet {
 	PImage imgOcean;
   PImage imgSeagull;
 
-  int seagullX = width / 2;
-  int seagullY = height / 2;
+  int seagullX = 600;
+  int seagullY = 120;
 
-  int xDirection = 10;
-  int yDirection = 10;
+  int xSpeedSeagull = 2;
+  int ySpeedSeagull = 2;
+
+  int cloudX = 10; 
+  int cloudY = 400;
+
+  int cloudSpeedX = -1;
+  int cloudSpeedY = -1;
  
   public void settings() {
-    size(800, 800);
+    size(750, 750);
   }
 
   public void setup() {
     imgOcean = loadImage("ocean.png");
-    imgOcean.resize(800, 800);
+    imgOcean.resize(750, 750);
 
     imgSeagull = loadImage("seagull.png");
-    imgSeagull.resize(150, 150);
+    imgSeagull.resize(120, 120);
   }
 
   public void draw() {
@@ -29,18 +35,32 @@ public class Sketch extends PApplet {
 
     image(imgSeagull, seagullX, seagullY);
 
-    seagullX = seagullX - xDirection;
-    seagullY = seagullY + yDirection;
+    seagullX = seagullX - xSpeedSeagull;
+    seagullY = seagullY + ySpeedSeagull;
 
-    if ((seagullX / 2) == 0 || seagullX == 800){
-      xDirection = xDirection * -1;
+    if (seagullX / 2 == 0 || seagullX == 630){
+      xSpeedSeagull = xSpeedSeagull * -1;
     }
 
-    if ((seagullY / 2) == 0 || seagullY == 800){
-      yDirection = yDirection * -1;
+    if (seagullY / 2 == 0 || seagullY == 630){
+      ySpeedSeagull = ySpeedSeagull * -1;
+    } 
+
+    noStroke();
+
+    ellipse((int) cloudX, (int) cloudY + 20, 120, 75);
+    ellipse((int) cloudX + 30, (int) cloudY, 135, 90);
+    ellipse((int) cloudX - 30, (int) cloudY - 10, 120, 100);
+    
+    cloudX = cloudX - cloudSpeedX;
+    cloudY = cloudY + cloudSpeedY;
+
+    if (cloudX / 2 < 0 || cloudX > 650){
+      cloudSpeedX = cloudSpeedX * -1;
     }
 
-
+    if (cloudY / 2 < 200 || cloudY > 560){
+      cloudSpeedY = cloudSpeedY * -1;
+    } 
   }
-  
 }
